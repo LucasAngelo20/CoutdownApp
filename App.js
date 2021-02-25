0603daec7a6acc8765e109971b80b7f265b8c536
Lucas Angelo <lucasg258@gmail.com>
2021-02-25 10:48:40 -0300

    Finished

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
   View,
   StyleSheet,
 } from 'react-native';
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 const colors = {
   black: '#323F4E',
   red: '#F76A6A',
@@ -24,53 +24,92 @@ const ITEM_SIZE = width * 0.38;
 const ITEM_SPACING = (width - ITEM_SIZE) / 2;
 
 export default function App() {
-  const scrollX = React.useRef(new Animated.Value(0)).current
-  const [duration, setDuration] =  React.useState(timers[0]);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const [duration, setDuration] = React.useState(timers[0]);
+  const inputRef = React.useRef();
   const timerAnimation = React.useRef(new Animated.Value(height)).current;
+  const textInputAnimation = React.useRef(new Animated.Value(timers[0]))
+    .current;
   const buttonAnimation = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    const listener = textInputAnimation.addListener(({value}) => {
+      inputRef?.current?.setNativeProps({
+        text: Math.ceil(value).toString(),
+      });
+    });
+
+    return () => {
+      textInputAnimation.removeListener(listener);
+      textInputAnimation.removeAllListeners();
+    };
+  });
+
   const animation = React.useCallback(() => {
-
+    textInputAnimation.setValue(duration)
     Animated.sequence([
-
       Animated.timing(buttonAnimation, {
         toValue: 1,
-        furation:300,
-        useNativeDriver: true
+        furation: 300,
+        useNativeDriver: true,
       }),
       Animated.timing(timerAnimation, {
         toValue: 0,
-        furation:300,
-        useNativeDriver: true
+        furation: 300,
+        useNativeDriver: true,
       }),
-      Animated.timing(timerAnimation, {
-        toValue: height,
-        duration: duration * 1000,
-        useNativeDriver: true
-      })
+      Animated.parallel([
+        Animated.timing(textInputAnimation, {
+          toValue: 0,
+          duration: duration * 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(timerAnimation, {
+          toValue: height,
+          duration: duration * 1000,
+          useNativeDriver: true,
+        })
+      ]),
+      Animated.delay(400)
     ]).start(() => {
-      buttonAnimation.setValue(0);
-    })
-  }, [duration])
+      textInputAnimation.setValue(duration)
+      Animated.timing(buttonAnimation, {
+        setValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    });
+  }, [duration]);
 
   const opacity = buttonAnimation.interpolate({
-    inputRange:[0, 1],
-    outputRange:[1, 0]
-  })
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  });
   const translateY = buttonAnimation.interpolate({
-    inputRange:[0, 1],
-    outputRange:[0, 200]
-  })
+    inputRange: [0, 1],
+    outputRange: [0, 200],
+  });
+  const textOpacity = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+  });
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Animated.View style={[StyleSheet.absoluteFillObject, {
-        height,
-        width,
-        backgroundColor:colors.red,
-        transform:[{
-          translateY: timerAnimation
-        }]
-      }]}
+      <Animated.View
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            height,
+            width,
+            backgroundColor: colors.red,
+            transform: [
+              {
+                translateY: timerAnimation,
+              },
+            ],
+          },
+        ]}
       />
       <Animated.View
         style={[
@@ -78,19 +117,17 @@ export default function App() {
           {
             justifyContent: 'flex-end',
             alignItems: 'center',
-            paddingBottom: 100,
+            paddingBottom: 110,
             opacity,
-            transform:[{
-              translateY
-            }]
+            transform: [
+              {
+                translateY,
+              },
+            ],
           },
-          
         ]}>
-        <TouchableOpacity
-          onPress={animation}>
-          <View
-            style={styles.roundButton}
-          />
+        <TouchableOpacity onPress={animation}>
+          <View style={styles.roundButton} />
         </TouchableOpacity>
       </Animated.View>
       <View
@@ -101,55 +138,83 @@ export default function App() {
           right: 0,
           flex: 1,
         }}>
-          <Text style={styles.text}>{duration}</Text>
-          <Animated.FlatList
+        <Animated.View
+          style={{
+            position: 'absolute',
+            width: ITEM_SIZE,
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+            opacity: textOpacity,
+          }}>
+          <TextInput
+            ref={inputRef}
+            style={styles.text}
+            defaultValue={duration.toString()}
+          />
+        </Animated.View>
+        <Animated.FlatList
+          data={timers}
+          keyExtractor={(item) => item.toString()}
+          horizontal
+          bounces={false}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {x: scrollX}}}],
+            {useNativeDriver: true},
+          )}
+          onMomentumScrollEnd={(ev) => {
+            const index = Math.round(
+              ev.nativeEvent.contentOffset.x / ITEM_SIZE,
+            );
+            setDuration(timers[index]);
+          }}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={ITEM_SIZE}
+          style={{flexGrow: 0, opacity}}
+          contentContainerStyle={{
+            paddingHorizontal: ITEM_SPACING,
+          }}
+          renderItem={({item, index}) => {
+            const inputRange = [
+              (index - 1) * ITEM_SIZE,
+              index * ITEM_SIZE,
+              (index + 1) * ITEM_SIZE,
+            ];
 
-            data={timers}
-            keyExtractor={item => item.toString()}
-            horizontal
-            bounces={false}
-            onScroll={Animated.event(
-              [{nativeEvent: {contentOffset: {x: scrollX}}}],
-              {useNativeDriver: true}
-            )}
-            onMomentumScrollEnd={ev => {
-              const index = Math.round(ev.nativeEvent.contentOffset.x / ITEM_SIZE)
-              setDuration(timers[index]);
-            }}
-            showsHorizontalScrollIndicator={false}
-            snapToInterval={ITEM_SIZE}
-            style={{flexGrow:0}}
-            contentContainerStyle={{
-              paddingHorizontal: ITEM_SPACING
-            }}
-            renderItem={({item, index}) => {
-              const inputRange = [
-                (index - 1) * ITEM_SIZE,
-                index * ITEM_SIZE,
-                (index + 1) * ITEM_SIZE,
-              ]
-
-              const opacity = scrollX.interpolate({
-                inputRange,
-                outputRange: [.4,1,.4]
-              })
-              const scale = scrollX.interpolate({
-                inputRange,
-                outputRange: [.7,1,.7]
-              })
-              return <View style={{width: ITEM_SIZE, justifyContent: 'center', alignItems:'center'}}>
-                <Animated.Text style={[styles.text, {
-                  opacity,
-                  transform:[{
-                    scale
-                  }]
-                }]}>
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.4, 1, 0.4],
+            });
+            const scale = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.7, 1, 0.7],
+            });
+            return (
+              <View
+                style={{
+                  width: ITEM_SIZE,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Animated.Text
+                  style={[
+                    styles.text,
+                    {
+                      opacity,
+                      transform: [
+                        {
+                          scale,
+                        },
+                      ],
+                    },
+                  ]}>
                   {item}
                 </Animated.Text>
-                </View>
-            }}
-          />
-        </View>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -170,5 +235,5 @@ const styles = StyleSheet.create({
     fontFamily: 'Menlo',
     color: colors.text,
     fontWeight: '900',
-  }
+  },
 });
